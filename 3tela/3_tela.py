914ed1c59ebec56f93b132bd160675c84907f46d
Andreid666 <Levin.Andrei@phystech.edu>
2019-09-24 14:17:34 +0300

    Add code for window

---
+++ 3tela/3_tela.py
@@ -0,0 +1,34 @@
+import math as m
+import graphics as gr
+
+w = gr.GraphWin("Window",1000, 600)
+
+def main(w):
+    pass
+# def raschet( x2, y2, x3, y3, vx1, dt ):
+
+# 	a = 
+
+# 	vx1 = vx1 - a*( x2 / ( x2**2 + y2**2 )**(1.5) - x3 / ( x3**2 + y3**2 )**(1.5)*dt
+
+# 	dx1 = vx1*dt
+
+
+# #star:
+# def star(x,y):
+# 	s = gr.Circle(gr.Point(x, y),50)
+# 	s.setFill("blue")
+# 	s.draw( w )
+
+
+# #begin!
+# p1 = star (x1, y1)
+# p2 = star (x2, y2)
+# p3 = star (x3, y3)
+# for i in range(1000):
+
+main(w)
+
+w.getMouse()
+w.close()
+
