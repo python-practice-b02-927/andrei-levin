92c3eacb4723f87335b56d4f4639a2ab48714e74
Andreid666 <Levin.Andrei@phystech.edu>
2019-10-08 02:32:43 +0300

    Conditionals

--- lab3_2/ball.py
+++ lab3_2/ball.py
@@ -1,18 +1,30 @@
 import graphics as gr
 import math as m
 
-
-conditional = {}
+g = 9.8
+speed = {
+	"vx" : 0,
+	"vy" : 0
+}
 
 def create_ball():
 	ball = gr.Circle(gr.Point(15, 580), 7)
 	ball.setFill('red')
 	ball.draw(w)
 
+def conditional(speed):
+	print('force:')
+	v = int(input())
+	print('angle:')
+	fi = int(input()) *m.pi/180
+	speed['vx'] = v*m.cos(fi)
+	speed['vy'] = v*m.sin(fi)
+
+
 def main():
-		conditional['force'] = int(input())
-		conditional['angle'] = int(input())
-		print(conditional)
+	conditional(speed)
+	print(speed)
+
 
 
 
